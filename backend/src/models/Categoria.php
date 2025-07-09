75622cc8b8013db50a44ef3688983d4600134638
Gabriel <gabrieusilvaa14@gmail.com>
2025-07-09 11:22:35 -0300

    pronto, so falta testar

--- backend/src/models/Categoria.php
+++ backend/src/models/Categoria.php
@@ -69,4 +69,35 @@ class Categoria {
             return false;
         }
     }
+
+    public function atualizar(int $id, array $dados) {
+        $sql = 'UPDATE categorias SET nome_categoria = :nome_categoria WHERE id_categoria = :id';
+        
+        try {
+            $conn = \Garden\Core\Database::getInstance();
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->bindParam(':nome_categoria', $dados['nome_categoria']);
+
+            return $stmt->execute();
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
+
+    public function excluir($id): bool {
+        $sql = 'DELETE FROM categorias WHERE id_categoria = :id';
+
+        try {
+            $conn = \Garden\Core\Database::getInstance();
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            return$stmt->execute();
+        } catch (\PDOException $e) {
+            return false;
+        }
+    }
 }
