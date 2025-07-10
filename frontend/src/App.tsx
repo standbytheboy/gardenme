e9655efa61554c275d299fd4ec75cdd5651312d3
standby <moraislcs05@gmail.com>
2025-07-10 14:25:28 -0300

    order sucess page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import Footer from './components/footer/Footer';
-import CheckoutPage from './components/checkoutPage/CheckoutPage'; // Importe o novo componente
+import OrderSuccessPage from './components/orderSucessPage/OrderSucessPage.tsx'; // Importe o novo componente
 import './index.css';
 
 function App() {
   return (
-    <div className="App flex flex-col min-h-screen">
-      <main className="flex-grow">
-        <CheckoutPage /> {/* Renderize a tela de finalizar compra */}
-      </main>
-      <Footer />
+    <div className="App">
+      <OrderSuccessPage orderNumber="123456789" /> {/* Passe um número de pedido opcional */}
     </div>
   );
 }
