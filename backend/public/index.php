aa770bf55c5f777d6e02f825b3633846ccdae6d6
standbytheboy <moraislcs05@gmail.com>
2025-08-27 16:52:26 -0300

    organizing files

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,7 +7,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit();
