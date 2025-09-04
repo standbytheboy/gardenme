7eec897099104a859ac64cd1d9430cbad3c59cd1
standbytheboy <moraislcs05@gmail.com>
2025-09-04 15:37:31 -0300

    making header component responsive

--- frontend/src/pages/OrderSucessPage.tsx
+++ frontend/src/pages/OrderSucessPage.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import successIllustration from "../assets/Reforestation-cuate.svg"; // Substitua pelo caminho da sua imagem real
+import successIllustration from "../assets/Reforestation-cuate.svg";
 import { Navbar } from "../components/Navbar";
 import Footer from "../components/Footer";
-import { useNavigate } from "react-router-dom";
-import { OrderSuccessPageProps } from "src/components/interfaces";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const OrderSuccessPage: React.FC<OrderSuccessPageProps> = ({
-  orderNumber = "12345",
-}) => {
-  
+const OrderSuccessPage: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Captura o número do pedido passado pelo CheckoutPage
+  const orderNumber = location.state?.orderNumber || "00000";
 
   return (
-    <div className="mt-21">
-      <Navbar></Navbar>
-      <div className="min-h-[15rem] bg-[#386641] text-center flex flex-col items-center justify-center p-8 md:p-20">
-        <h1 className="text-4xl md:text-5xl font-bold text-[#A7C957] mb-6">
+    <div>
+      <Navbar />
+      <div className="min-h-[45rem] bg-[#386641] px-6 py-12 md:px-20 md:py-20 flex flex-col items-center text-center md:text-left">
+        <h1 className="text-3xl md:text-5xl font-bold text-[#A7C957] mb-6 mt-6 md:mt-0">
           Pedido Realizado com Sucesso!
         </h1>
 
-        <p className="text-lg md:text-xl text-[#D4EDC8] max-w-3xl leading-relaxed mb-12">
+        <p className="text-base md:text-xl text-[#D4EDC8] max-w-3xl leading-relaxed mb-12">
           Agradecemos a sua compra! Seu pedido #
           <span className="font-semibold">{orderNumber}</span> foi confirmado e
           está sendo preparado com todo carinho. Em breve, a natureza estará
@@ -30,26 +30,32 @@ const OrderSuccessPage: React.FC<OrderSuccessPageProps> = ({
           <img
             src={successIllustration}
             alt="Pedido Realizado"
-            className="mx-auto size-[20rem]"
+            className="mx-auto w-64 md:w-80"
           />
         </div>
 
-        <p className="text-md md:text-lg text-[#D4EDC8] max-w-3xl leading-relaxed mb-10">
+        <p className="text-sm md:text-lg text-[#D4EDC8] max-w-3xl leading-relaxed mb-10">
           Você receberá um e-mail com os detalhes e o acompanhamento da entrega.
           Enquanto isso, explore mais plantas ou prepare o cantinho da sua nova
           amiga verde!
         </p>
 
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 w-full max-w-lg">
-          <button onClick={() => navigate('/perfil')} className="cursor-pointer flex-1 bg-[#A7C957] text-[#386641] py-3 px-6 rounded-full text-lg font-semibold hover:opacity-90 transition duration-300 shadow-lg">
+        <div className="flex flex-col md:flex-row gap-4 md:gap-6 w-full max-w-lg mb-10">
+          <button
+            onClick={() => navigate("/perfil")}
+            className="cursor-pointer flex-1 bg-[#A7C957] text-[#386641] py-3 px-6 rounded-full text-base md:text-lg font-semibold hover:opacity-90 transition duration-300 shadow-lg"
+          >
             Ver Meus Pedidos
           </button>
-          <button onClick={() => navigate('/plantas')} className="cursor-pointer flex-1 bg-[#A7C957] text-[#386641] py-3 px-6 rounded-full text-lg font-semibold hover:opacity-90 transition duration-300 shadow-lg">
+          <button
+            onClick={() => navigate("/plantas")}
+            className="cursor-pointer flex-1 bg-[#A7C957] text-[#386641] py-3 px-6 rounded-full text-base md:text-lg font-semibold hover:opacity-90 transition duration-300 shadow-lg"
+          >
             Continuar Comprando
           </button>
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
